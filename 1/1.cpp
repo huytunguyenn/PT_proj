f9a5e7cc569c1ba5c7d898c78b64b2f1434e533d
huytunguyen <dragon10x0408@gmail.com>
2019-04-19 23:57:23 +0700

    Update 1.cpp

--- 1/1.cpp
+++ 1/1.cpp
@@ -2,32 +2,30 @@
 //
 
 #include "stdafx.h"
+#define MAX_PASS 25 
 //biến toàn cục
-char password[10] = { "userpass" };
+char passAdmin[20] = { "admin" };
+char passQuanLy[20] = { "quanly" };
+char passChuyenVien[20] = { "chuyenvien" };
 
-void Login()
-{
-	// vũ viết hàm
-}
-void Logout()
-{
-	// vũ viết hàm
-}
+
 void ChangePass()
 {
-	// vũ viết hàm
 }
 void UpadateInfo()
 {
-	// tú viết hàm
 }
 void CreateUser(/*DATABASE &data, USER *p*/)
 {
-	// tú viết hàm
 	/*if (strcmp(it.Acc.sUsername, user.Acc.sUsername) == 0)*/
 
 }
-void gotoxy(int x, int y) // dùng để trang trí, chạy hiệu ứng
+void Authority()
+{
+
+}
+// dùng để trang trí, chạy hiệu ứng
+void gotoxy(int x, int y)
 {
 	static HANDLE h = NULL;
 	if (!h)
@@ -35,146 +33,77 @@ void gotoxy(int x, int y) // dùng để trang trí, chạy hiệu ứng
 	COORD c = { x, y };
 	SetConsoleCursorPosition(h, c);
 }
-void mainmenu()
-{
-	gotoxy(20, 7);
-	printf("\t1. Dang nhap");
-	gotoxy(20, 9);
-	printf("\t2. Dang xuat");
-	gotoxy(20, 11);
-	printf("\t3. Thay doi mat khau");
-	gotoxy(20, 13);
-	printf("\t4. Cap nhat thong tin ca nhan");
-	gotoxy(20, 15);
-	printf("\t5. Tao nguoi dung");
-	gotoxy(20, 17);
-	printf("\t6. Phan quyen nguoi dung (danh cho quan ly, nhan vien)");
-	gotoxy(20, 19);
-	printf("\t7. Thoat chuong trinh");
 
-	gotoxy(20, 22);
-	printf("Lua chon cua ban:");
-	switch (_getch())
-	{
-	case '1':
-		Login();
-		break;
-	case '2':
-		Logout();
-		break;
-	case '3':
-		ChangePass();
-		break;
-	case '4':
-		UpadateInfo();
-		break;
-	case '5':
-		CreateUser();
-		break;
-	case '6':
-
-		break;
-	case '7':
-	{
-		system("cls");
-		gotoxy(20, 11);
-		printf("Thoat trong 3 giay nua");
-		for (int j = 0; j < 60; j++)
-		{
-			Sleep(50);
-			printf(".");
-		}
-		//Sleep(3000); // ngủ 3 giây
-		system("cls");
-		exit(0);
-	}
-	default:
-	{
-		gotoxy(20, 24);
-		printf("\aChon sai, vui long chon lai!");
-		if (_getch())
-			mainmenu();
-	}
-	}
-}
-void MainMenuAdmin()
-{
-
-	system("cls");
-	int i;
-	gotoxy(20, 3);
-	printf("\xB2\xB2\xB2\xB2\xB2\xB2 HE THONG QUAN LY THU VIEN \xB2\xB2\xB2\xB2\xB2\xB2");
-	gotoxy(20, 5);
-	printf("\xB2\xB2\xB2\ 1. Dang xuat   ");
-	gotoxy(20, 7);
-	printf("\xB2\xB2\xB2\ 2. Thay doi mat khau   ");
-	gotoxy(20, 9);
-	printf("\xB2\xB2\xB2\ 3. ");
-	gotoxy(20, 11);
-	printf("\xB2\xB2\xB2\ 4. Cap nhat thong tin ca nhan");
-	gotoxy(20, 13);
-	printf("\xB2\xB2\xB2\ 5. View Books List");
-	gotoxy(20, 15);
-	printf("\xB2\xB2\xB2\ 6. Edit Book's Record");
-	gotoxy(20, 17);
-	printf("\xB2\xB2\xB2\ 7. Thoat chuong trinh");
-	gotoxy(20, 19);
-	printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
-	gotoxy(20, 20);
-
-	gotoxy(20, 21);
-	printf("Lua chon cua ban:");
-	switch (_getch())
-	{
-	case '1':
-	
-		break;
-	case '2':
-	
-		break;
-	case '3':
-
-		break;
-	case '4':
-
-		break;
-	case '5':
-
-		break;
-	case '6':
-
-		break;
-	case '7':
-	{
-		system("cls");
-		printf("Thoat trong 3 giay nua");
-		for (int j = 0; j < 20; j++)
-		{
-			Sleep(50);
-			printf(".");
-		}
-		Sleep(3000); // sleep three seconds
-		system("cls");
-		exit(0);
-	}
-	default:
-	{
-		gotoxy(10, 23);
-		printf("\aChon sai, vui long chon lai!");
-		if (_getch())
-			MainMenuAdmin();
-	}
-	}
-}
-void Password(void) //để đăng nhập
+void Login(int &luachon)
 {
 	system("cls");
+	char ch, pass[MAX_PASS];
+	gotoxy(10, 4); printf("Ten dang nhap: ");
+	//ch = _getch();
+	gotoxy(10, 5); printf("Password:\n");
+	// mã hóa mật khẩu
+	gotoxy(19, 5);
+	int p = 0;
+	do {
+		pass[p] = _getch();
+		if (pass[p] != '\r')
+		{
+			printf("*");
+		}
+		p++;
+	} while (pass[p - 1] != '\r');
+	pass[p - 1] = '\0';
+	if (strcmp(pass, passAdmin) == 0)
+	{
+		gotoxy(15, 9); printf("Ban da dang nhap duoi quyen admin.\n");
+	}
+	else if (strcmp(pass, passChuyenVien) == 0)
+	{
+		gotoxy(15, 9); printf("Ban da dang nhap duoi quyen chuyen vien.\n");
+	}
+	else if (strcmp(pass, passQuanLy) == 0)
+	{
+		gotoxy(15, 9); printf("Ban da dang nhap duoi quyen quan ly.\n");
+	}
+	else
+	{
+		gotoxy(8, 8); printf("\aDang nhap khong thanh cong!");
+		gotoxy(8, 9); printf("Quay lai menu chinh? (1 la co, 0 la khong):");
+		scanf("%d", &luachon);
+
+		if (luachon == 1)
+		{
+			system("cls");
+			// in lại nhưng không có hiệu ứng
+			char d[26] = "He Thong Quan Ly Thu Vien";
+			gotoxy(10, 4);
+			for (int j = 0; j < 20; j++)
+			{
+				printf(":");
+			}
+			for (int j = 0; j < 25; j++)
+			{
+				printf("%c", d[j]);
+			}
+			for (int j = 0; j < 20; j++)
+			{
+				printf(":");
+			}
+		}
+		else
+		{
+			Login(luachon);
+		}
+	}
+}
+
+void menu() //đăng nhập
+{
+	int luachon = 0; // nếu người dùng muốn quay lại: 1=có,0=ko
 	char d[26] = "He Thong Quan Ly Thu Vien";
-	char ch, pass[10];
-	int i = 0;
-	gotoxy(10, 4);
-
+	system("cls");
 	// in chuyển động 
+	gotoxy(10, 4);
 	for (int j = 0; j < 20; j++)
 	{
 		Sleep(50);
@@ -190,38 +119,35 @@ void Password(void) //để đăng nhập
 		Sleep(50);
 		printf(":");
 	}
-	
-	
-	mainmenu();
-
-	/*while (ch != 13)
-	{
-		ch = _getch();
-
-		if (ch != 13 && ch != 8) {
-			_putch('*');
-			pass[i] = ch;
-			i++;
+	do {
+		gotoxy(20, 7); printf("\t1.Dang nhap");
+		gotoxy(20, 9); printf("\t2.Thoat chuong trinh");
+		gotoxy(20, 11); printf("Lua chon cua ban:");
+		switch (_getch())
+		{
+		case '1':
+			Login(luachon);
+			break;
+		case '2':
+		{
+			system("cls");
+			gotoxy(20, 11); printf("Thoat trong 3 giay nua");
+			for (int j = 0; j < 60; j++)
+			{
+				Sleep(50);
+				printf(".");
+			}
+			system("cls");
+			exit(0);
 		}
-	}
-	pass[i] = '\0';
-	if (strcmp(pass, password) == 0)
-	{
-
-		gotoxy(15, 9);
-		printf("Ban da dang nhap bang quyen admin!");
-		gotoxy(17, 10);
-		printf("\n\xB2\xB2\xB2\xB2\Press any key to countinue...");
-		_getch();
-		MainMenuAdmin();
-	}
-	else
-	{
-		gotoxy(15, 16);
-		printf("\aMat khau khong chinh xac!");
-		_getch();
-		Password();
-	}*/
+		default:
+		{
+			gotoxy(20, 24); printf("\aChon sai, vui long chon lai!");
+			if (_getch())
+				menu();
+		}
+		}
+	} while (luachon == 1);
 }
 
 //void MainMenuQuanLy()
@@ -375,14 +301,82 @@ void Password(void) //để đăng nhập
 //	}
 //}
 
-
+//void MenuAdmin()
+//{
+//
+//	system("cls");
+//	int i;
+//	gotoxy(20, 3);
+//	printf("\xB2\xB2\xB2\xB2\xB2\xB2 HE THONG QUAN LY THU VIEN \xB2\xB2\xB2\xB2\xB2\xB2");
+//	gotoxy(20, 5);
+//	printf("\xB2\xB2\xB2\ 1. Dang xuat   ");
+//	gotoxy(20, 7);
+//	printf("\xB2\xB2\xB2\ 2. Thay doi mat khau   ");
+//	gotoxy(20, 9);
+//	printf("\xB2\xB2\xB2\ 3. ");
+//	gotoxy(20, 11);
+//	printf("\xB2\xB2\xB2\ 4. Cap nhat thong tin ca nhan");
+//	gotoxy(20, 13);
+//	printf("\xB2\xB2\xB2\ 5. View Books List");
+//	gotoxy(20, 15);
+//	printf("\xB2\xB2\xB2\ 6. Edit Book's Record");
+//	gotoxy(20, 17);
+//	printf("\xB2\xB2\xB2\ 7. Thoat chuong trinh");
+//	gotoxy(20, 19);
+//	printf("\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+//	gotoxy(20, 20);
+//
+//	gotoxy(20, 21);
+//	printf("Lua chon cua ban:");
+//	switch (_getch())
+//	{
+//	case '1':
+//
+//		break;
+//	case '2':
+//
+//		break;
+//	case '3':
+//
+//		break;
+//	case '4':
+//
+//		break;
+//	case '5':
+//
+//		break;
+//	case '6':
+//
+//		break;
+//	case '7':
+//	{
+//		system("cls");
+//		printf("Thoat trong 3 giay nua");
+//		for (int j = 0; j < 20; j++)
+//		{
+//			Sleep(50);
+//			printf(".");
+//		}
+//		Sleep(3000); // sleep three seconds
+//		system("cls");
+//		exit(0);
+//	}
+//	default:
+//	{
+//		gotoxy(10, 23);
+//		printf("\aChon sai, vui long chon lai!");
+//		if (_getch())
+//			MenuAdmin();
+//	}
+//	}
+//}
 
 
 int main()
 {
-	system("color F0");
-	Password();
+	//system("color F0");
+	menu();
 	_getch();
-    return 0;
+	return 0;
 }
 
